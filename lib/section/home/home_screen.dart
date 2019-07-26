b643a9b7cddf9859361efc83e7a74b295bbbfa0f
Irfan Pertadima <pertadimairfan@gmail.com>
2019-07-26 13:56:20 +0700

    [irfan] load image

--- lib/section/home/home_screen.dart
+++ lib/section/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_db/bloc/home/home_bloc.dart';
+import 'package:movie_db/bloc/home/home_event.dart';
 import 'package:movie_db/bloc/home/home_state.dart';
 import 'package:movie_db/network/network_repository.dart';
 
@@ -14,32 +15,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final HomeBloc _homeBloc =
       HomeBloc(networkRepository: NetworkRepository.get());
-      
+
+  @override
+  void initState() {
+    super.initState();
+    _homeBloc.dispatch(FetchTrendingMovie());
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder(
       bloc: _homeBloc,
       builder: (BuildContext context, HomeState homeState) {
         if (homeState is HomeLoaded) {
-          
+          return BlocProviderTree(
+            blocProviders: [
+              BlocProvider<HomeBloc>(
+                bloc: _homeBloc,
+              ),
+            ],
+            child: Scaffold(
+              resizeToAvoidBottomPadding: false,
+              backgroundColor: Colors.white,
+              appBar: AppBar(title: Text('Movie DB - Flutter')),
+              body: Container(
+                  height: double.infinity,
+                  child: GridView.builder(
+                    padding: EdgeInsets.all(20.0),
+                    itemBuilder: (context, position) {
+                      return Card(
+                        child: Image.network(
+                            "https://image.tmdb.org/t/p/w500${homeState.movie.results[position].posterPath}",
+                            fit: BoxFit.cover),
+                      );
+                    },
+                    itemCount: homeState.movie.results.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                  )),
+            ),
+          );
         }
 
-        return BlocProviderTree(
-          blocProviders: [
-            BlocProvider<HomeBloc>(
-              bloc: _homeBloc,
-            ),
-          ],
-          child: Scaffold(
-            resizeToAvoidBottomPadding: false,
-            backgroundColor: Colors.white,
-            appBar: AppBar(title: Text('Movie DB - Flutter')),
-            body: Container(
-              height: double.infinity,
-              child: null
-            ),
-          ),
-        );
+        if (homeState is HomeError) {}
+
+        if (homeState is HomeLoading) {}
       },
     );
   }
